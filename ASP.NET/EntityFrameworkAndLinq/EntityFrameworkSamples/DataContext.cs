99e6fadcf1273e21dd08c6caed95e18a6bdb80a2
mebaysan <menesbaysan@gmail.com>
2020-03-01 01:27:08 +0300

    entity framework temelleri

---
+++ ASP.NET/EntityFrameworkAndLinq/EntityFrameworkSamples/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkSamples
+{
+    public class DataContext : DbContext
+    {
+        //public DataContext():base("CustomDatabaseName") // içine connection string veya istediğimiz bir veritabanı adı girebiliriz (opsiyonel)
+        public DataContext() : base("urunConnection") // istersek içine connection string verebiliriz (App.config altında)
+        {
+            Database.SetInitializer(new DataInitializer()); // Veritabanı oluşturulurken hangi Initializer'ı kullanacağını set ettik. Bu sayede ilk Context instance oluştuğunda belirttiğimiz Initializer çalışacak ve Data Seeding gerçekleşecek
+        }
+
+
+        public DbSet<Kategori> Kategoriler { get; set; } // Kategori tipinde datalarla işlem yapacağımız için belirttik. DbSet türünde olmalı(bir nevi list)
+        public DbSet<Urun> Urunler { get; set; }
+    }
+}
